0441f2c82e0c15fa29aff8ed1169b768c5eea8bb
Max <e1426820@student.tuwien.ac.at>
2016-05-19 12:40:13 +0200

    Service Implementation ready...not tested

---
+++ assignment/src/main/java/at/ac/tuwien/big/we16/ue3/service/ProductSoldRestResponse.java
@@ -0,0 +1,41 @@
+package at.ac.tuwien.big.we16.ue3.service;
+
+import java.util.Date;
+import java.util.DoubleSummaryStatistics;
+
+/**
+ * Created by Max on 19.05.2016.
+ */
+public class ProductSoldRestResponse {
+    String name;
+    String product;
+    String price;
+    String date;
+    String id;
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getProduct() {
+        return product;
+    }
+
+
+    public String getPrice() {
+        return price;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+
+}
